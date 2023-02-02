9e671f147b1a75017e5d62afa8fcbda5859b27ef
Shengdong Zhang <zhangshengdongofgz@gmail.com>
2023-02-02 05:02:30 -0500

    Update SMORMS3_pytorch.py

--- SMORMS3_pytorch.py
+++ SMORMS3_pytorch.py
@@ -1,37 +1,54 @@
 # Conversion from Keras to Pytorch done by ChatGPT. Use with caution.
 import torch
+from torch.optim import Optimizer
 
-class SMORMS3:
-    def __init__(self, params, lr=0.001, epsilon=1e-16, decay=0):
-        self.params = list(params)
-        self.lr = lr
-        self.epsilon = epsilon
-        self.decay = decay
-        self.iterations = 0
 
-        self.g1s = [torch.zeros_like(p) for p in self.params]
-        self.g2s = [torch.zeros_like(p) for p in self.params]
-        self.mems = [torch.ones_like(p) for p in self.params]
+class SMORMS3(Optimizer):
+    def __init__(self, params, lr=1e-3, epsilon=1e-16, weight_decay=0, **kwargs):
+        defaults = dict(lr=lr, epsilon=epsilon, weight_decay=weight_decay)
+        super(SMORMS3, self).__init__(params, defaults)
 
     def step(self, closure=None):
         loss = None
         if closure is not None:
             loss = closure()
 
-        self.iterations += 1
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            lr = group['lr']
+            epsilon = group['epsilon']
 
-        lr = self.lr * (1. / (1. + self.decay * self.iterations))
+            for p in group['params']:
+                if p.grad is None:
+                    continue
 
-        for p, g1, g2, m in zip(self.params, self.g1s, self.g2s, self.mems):
-            g = p.grad.data
-            r = 1. / (m + 1)
-            new_g1 = (1. - r) * g1 + r * g
-            new_g2 = (1. - r) * g2 + r * g.pow(2)
-            p.data -= g * torch.min(lr, new_g1.pow(2) / (new_g2 + self.epsilon)) / (
-                    torch.sqrt(new_g2) + self.epsilon)
+                state = self.state[p]
 
-            g1.data = new_g1
-            g2.data = new_g2
-            m.data = 1 + m * (1 - new_g1.pow(2) / (new_g2 + self.epsilon))
+                if len(state) == 0:
+                    state['g1'] = torch.zeros_like(p.data)
+                    state['g2'] = torch.zeros_like(p.data)
+                    state['m'] = torch.ones_like(p.data)
+                    state['t'] = 0
+
+                g = p.grad.data
+                g1 = state['g1']
+                g2 = state['g2']
+                m = state['m']
+                t = state['t']
+
+                r = 1. / (m + 1)
+                new_g1 = (1 - r) * g1 + r * g
+                new_g2 = (1 - r) * g2 + r * g ** 2
+                new_m = 1 + m * (1 - g1 ** 2 / (g2 + epsilon))
+                t += 1
+
+                p.data.add_(-lr * g * torch.min(1, g1 ** 2 / (g2 + epsilon)) / (torch.sqrt(g2) + epsilon))
+                p.data.add_(-weight_decay * p.data)  # weight decay
+
+                state['g1'].copy_(new_g1)
+                state['g2'].copy_(new_g2)
+                state['m'].copy_(new_m)
+                state['t'] = t
 
         return loss
+
